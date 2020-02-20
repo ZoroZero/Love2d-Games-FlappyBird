2ba549823057115b114f54fb1eb7757c7e14315a
ZoroZero <40843946+ZoroZero@users.noreply.github.com>
2020-02-20 20:45:57 +0700

    Add bird class, gravity and jump

---
+++ Bird.lua
@@ -0,0 +1,23 @@
+Bird = Class{}
+
+function Bird:init()
+    self.sprite = love.graphics.newImage('bird.png')
+    self.width = self.sprite:getWidth()
+    self.height = self.sprite:getHeight()
+
+    self.x = VIRTUAL_WIDTH/2 - (self.width/2)
+    self.y = VIRTUAL_HEIGHT/2 - (self.height/2)
+    self.dy = 0;
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY_ACCELERATION*dt;
+    if love.keyboard.wasPressed('space') then
+        self.dy = JUMP_ACCELERATION;
+    end 
+    self.y = self.y + self.dy;
+end
+
+function Bird:render()
+    love.graphics.draw(self.sprite, self.x, self.y)
+end
--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
 push = require 'push'
 
+Class = require 'class'
+
+require 'Bird'
+
 WINDOW_WIDTH = 1280;
 WINDOW_HEIGHT = 720;
 
 VIRTUAL_WIDTH = 512;
 VIRTUAL_HEIGHT = 288;
 
+GRAVITY_ACCELERATION = 10;
+JUMP_ACCELERATION = -5;
 local background = love.graphics.newImage('background.png');
 local background_Scroll = 0;
 
@@ -15,8 +21,11 @@ local ground_Scroll = 0;
 local BACKGROUND_SCROLL_SPEED = 30;
 local GROUND_SCROLL_SPEED = 60;
 
-
 local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
+
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest');
     love.window.setTitle("Zero Bird");
@@ -26,6 +35,8 @@ function love.load()
         vsyn = true,
         fullscreen = false
     })
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -33,16 +44,24 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true;
     if key == 'escape' then
-        love.event.quit()
+        love.event.quit();
     end
 end
 
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key];
+end
 
 function love.update(dt)
     background_Scroll = (background_Scroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT;
 
     ground_Scroll = (ground_Scroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH;
+
+    bird:update(dt);
+
+    love.keyboard.keysPressed = {};
 end
 
 function love.draw()
@@ -51,6 +70,8 @@ function love.draw()
     love.graphics.draw(background, -background_Scroll, 0);
 
     love.graphics.draw(ground, -ground_Scroll, VIRTUAL_HEIGHT - 16);
+
+    bird:render()
 
     push:finish()
 end
